7cee8f4a700273a9e18f4e1a632e4da16906937e
Linnéa <linnea.ternevik@gmail.com>
2026-01-02 12:26:59 +0100

    Added signin feature

--- app/src/main/java/com/example/buzzly/viemodels/AuthViewModel.kt
+++ app/src/main/java/com/example/buzzly/viemodels/AuthViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.buzzly.viemodels
 
+import android.content.Intent
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.auth
@@ -17,4 +18,15 @@ class AuthViewModel : ViewModel() {
                 callback(resultTask)
             }
     }
+
+    fun signIn(email: String, password:String, onSuccess: ()-> Unit, onFailure: (Exception)-> Unit){
+
+        auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+            onSuccess()
+        }.addOnFailureListener {
+            onFailure(it)
+        }
+    }
+
+
 }
