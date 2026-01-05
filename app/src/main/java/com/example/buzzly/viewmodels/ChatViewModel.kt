870391e3d7138230827a230f30e80a721965faa3
Shallange <jimmy.kroneld@hotmail.com>
2026-01-05 18:53:27 +0100

    Add basic Firestore chat message structure

--- app/src/main/java/com/example/buzzly/viewmodels/ChatViewModel.kt
+++ app/src/main/java/com/example/buzzly/viewmodels/ChatViewModel.kt
@@ -2,8 +2,24 @@ package com.example.buzzly.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.firestore
 
 class ChatViewModel : ViewModel()  {
     val db = Firebase.firestore
+
+
+    fun sendMessage(roomId: String, text: String, senderId: String) {
+        val data = hashMapOf(
+            "text" to text,
+            "senderId" to senderId,
+            "timestamp" to FieldValue.serverTimestamp()
+        )
+
+        db.collection("chatRooms")
+            .document(roomId)
+            .collection("messages")
+            .add(data)
+    }
+
 }
