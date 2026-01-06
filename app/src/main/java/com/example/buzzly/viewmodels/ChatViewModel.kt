c0fe0d03300eacb06fc992b34d91f88a834b7a7f
Linnéa <linnea.ternevik@gmail.com>
2026-01-06 16:23:10 +0100

    Added ChatRepository and refactored  ChatViewModel

--- app/src/main/java/com/example/buzzly/viewmodels/ChatViewModel.kt
+++ app/src/main/java/com/example/buzzly/viewmodels/ChatViewModel.kt
@@ -1,24 +1,29 @@
 package com.example.buzzly.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.buzzly.data.ChatRepository
 import com.example.buzzly.data.Message
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.FieldValue
-import com.google.firebase.firestore.firestore
 
 class ChatViewModel : ViewModel()  {
-    val db = Firebase.firestore
+    private val repository = ChatRepository()
 
-    fun sendMessage(roomId: String, message: Message) {
-        val data = hashMapOf(
-            "text" to message.text,
-            "senderId" to message.senderId,
-            "timestamp" to FieldValue.serverTimestamp()
+    private val _currentRoomId = MutableLiveData<String>()
+    val currentRoomId: LiveData<String> = _currentRoomId
+
+    fun startChatWith(userId: String) {
+        repository.createChatWith(
+            otherUserId = userId,
+            onSuccess = { roomId ->
+                _currentRoomId.postValue(roomId)
+            }
         )
+    }
 
-        db.collection("chatRooms")
-            .document(roomId)
-            .collection("messages")
-            .add(data)
+    fun sendMessage(message: Message) {
+        val roomId = _currentRoomId.value ?: return
+        repository.sendMessage(roomId, message)
     }
 }
+
